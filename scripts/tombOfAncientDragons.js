c7237f9cd07dfb37f0e4fabd134999ee891a7c89
DVC-Wiki <146748943+DVC-Wiki@users.noreply.github.com>
2023-11-01 00:00:00 +0100

    location filters

--- scripts/tombOfAncientDragons.js
+++ scripts/tombOfAncientDragons.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Load XML data using XMLHttpRequest
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "xml/dragons.xml", true);
+    xhr.open("GET", "../xml/dragons.xml", true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var xmlDoc = xhr.responseXML;
@@ -40,7 +40,7 @@ function displayResults(dragons, searchTerm, locationFilter) {
     var tableBody = document.getElementById("tableBody");
     tableBody.innerHTML = "";
 
-   for (var i = 0; i < dragons.length; i++) {
+    for (var i = 0; i < dragons.length; i++) {
         var dragon = dragons[i];
         var dragonName = dragon.getElementsByTagName("dragonName")[0].textContent;
         var eggDescription = dragon.getElementsByTagName("eggDescription")[0].textContent;
@@ -55,82 +55,81 @@ function displayResults(dragons, searchTerm, locationFilter) {
         var spriteN = dragon.getElementsByTagName("spriteN")[0].textContent;
         var artworkURL = dragon.getElementsByTagName("artwork")[0].textContent;
 
-        // Check if any column contains the search term (case insensitive)
-        if (
-            dragonName.toLowerCase().includes(searchTerm) ||
-            eggDescription.toLowerCase().includes(searchTerm) ||
-            location.toLowerCase().includes(searchTerm) ||
-            careOptions.toLowerCase().includes(searchTerm) ||
-            personality1.toLowerCase().includes(searchTerm) ||
-            personality2.toLowerCase().includes(searchTerm)
-        ) {
-            var row = document.createElement("tr");
+        // Check if the dragon's location contains the location filter
+        if (location.toLowerCase().includes(locationFilter.toLowerCase())) {
+            // Check if any column contains the search term (case insensitive)
+            if (
+                dragonName.toLowerCase().includes(searchTerm) ||
+                eggDescription.toLowerCase().includes(searchTerm)
+            ) {
+                var row = document.createElement("tr");
 
-            var dragonNameCell = document.createElement("td");
-			dragonNameCell.innerHTML = dragonName; // Render as HTML
-			row.appendChild(dragonNameCell);
+                var dragonNameCell = document.createElement("td");
+                dragonNameCell.textContent = dragonName;
+                row.appendChild(dragonNameCell);
 
-            var eggDescriptionCell = document.createElement("td");
-            eggDescriptionCell.innerHTML = eggDescription;
-            row.appendChild(eggDescriptionCell);
+                var eggDescriptionCell = document.createElement("td");
+                eggDescriptionCell.textContent = eggDescription;
+                row.appendChild(eggDescriptionCell);
 
-            var locationCell = document.createElement("td");
-            locationCell.innerHTML = location;
-            row.appendChild(locationCell);
+                var locationCell = document.createElement("td");
+                locationCell.textContent = location;
+                row.appendChild(locationCell);
+				
+				var careOptionsCell = document.createElement("td");
+				careOptionsCell.innerHTML = careOptions; // Render as HTML
+				row.appendChild(careOptionsCell);
 
-            var careOptionsCell = document.createElement("td");
-            careOptionsCell.innerHTML = careOptions; // Render as HTML
-            row.appendChild(careOptionsCell);
+				var personality1Cell = document.createElement("td");
+				personality1Cell.innerHTML = personality1; // Render as HTML
+				row.appendChild(personality1Cell);
 
-            var personality1Cell = document.createElement("td");
-            personality1Cell.innerHTML = personality1; // Render as HTML
-            row.appendChild(personality1Cell);
+				var personality2Cell = document.createElement("td");
+				personality2Cell.innerHTML = personality2; // Render as HTML
+				row.appendChild(personality2Cell);
 
-            var personality2Cell = document.createElement("td");
-            personality2Cell.innerHTML = personality2; // Render as HTML
-            row.appendChild(personality2Cell);
+                var eggSpriteCell = document.createElement("td");
+                if (eggSprite) {
+                    var eggSpriteImg = document.createElement("img");
+                    eggSpriteImg.src = eggSprite;
+                    eggSpriteCell.appendChild(eggSpriteImg);
+                }
+                row.appendChild(eggSpriteCell);
 
-            var eggSpriteCell = document.createElement("td");
-            if (eggSprite) {
-                var eggSpriteImg = document.createElement("img");
-                eggSpriteImg.src = eggSprite;
-                eggSpriteCell.appendChild(eggSpriteImg);
+                var spriteMCell = document.createElement("td");
+                if (spriteM) {
+                    var spriteMImg = document.createElement("img");
+                    spriteMImg.src = spriteM;
+                    spriteMCell.appendChild(spriteMImg);
+                }
+                row.appendChild(spriteMCell);
+
+                var spriteFCell = document.createElement("td");
+                if (spriteF) {
+                    var spriteFImg = document.createElement("img");
+                    spriteFImg.src = spriteF;
+                    spriteFCell.appendChild(spriteFImg);
+                }
+                row.appendChild(spriteFCell);
+
+                var spriteNCell = document.createElement("td");
+                if (spriteN) {
+                    var spriteNImg = document.createElement("img");
+                    spriteNImg.src = spriteN;
+                    spriteNCell.appendChild(spriteNImg);
+                }
+                row.appendChild(spriteNCell);
+
+                var artworkCell = document.createElement("td");
+                if (artworkURL) {
+                    var artworkImg = document.createElement("img");
+                    artworkImg.src = artworkURL;
+                    artworkCell.appendChild(artworkImg);
+                }
+                row.appendChild(artworkCell);
+
+                tableBody.appendChild(row);
             }
-            row.appendChild(eggSpriteCell);
-
-            var spriteMCell = document.createElement("td");
-            if (spriteM) {
-                var spriteMImg = document.createElement("img");
-                spriteMImg.src = spriteM;
-                spriteMCell.appendChild(spriteMImg);
-            }
-            row.appendChild(spriteMCell);
-
-            var spriteFCell = document.createElement("td");
-            if (spriteF) {
-                var spriteFImg = document.createElement("img");
-                spriteFImg.src = spriteF;
-                spriteFCell.appendChild(spriteFImg);
-            }
-            row.appendChild(spriteFCell);
-
-            var spriteNCell = document.createElement("td");
-            if (spriteN) {
-                var spriteNImg = document.createElement("img");
-                spriteNImg.src = spriteN;
-                spriteNCell.appendChild(spriteNImg);
-            }
-            row.appendChild(spriteNCell);
-
-            var artworkCell = document.createElement("td");
-            if (artworkURL) {
-                var artworkImg = document.createElement("img");
-                artworkImg.src = artworkURL;
-                artworkCell.appendChild(artworkImg);
-            }
-            row.appendChild(artworkCell);
-
-            tableBody.appendChild(row);
         }
     }
 }
